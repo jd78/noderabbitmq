a964864a7130fd3d8540b056f1ebf50f7155073d
Gennaro Del Sorbo <JD78@JD-macbook.local>
2017-10-27 11:58:53 +0200

    Publish

--- test/integration.js
+++ test/integration.js
@@ -5,8 +5,11 @@ let initialize = async () => {
     await index.initialize({
         serverUrl: 'amqp://localhost'
     })
-    connectionManager.exchangeDeclare('test', 'fanout', true)
+    connectionManager.exchangeDeclare('test', 'topic', true)
+    connectionManager.queueDeclare('test.inbound', true, false, {"x-message-ttl" : 20000})
+    connectionManager.queueBind('test.inbound', 'test', '#')
+
+    connectionManager.publish('test', null, null, 'key', 'some message')
 }
 
 initialize()
-
