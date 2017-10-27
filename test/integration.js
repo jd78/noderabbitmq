caf51e5930bbb2053c8c79787fea89424992f98b
Gennaro Del Sorbo <JD78@JD-macbook.local>
2017-10-27 12:13:12 +0200

    Publish confirms

--- test/integration.js
+++ test/integration.js
@@ -3,13 +3,14 @@ const connectionManager = require('../lib/connection-manager')
 
 let initialize = async () => {
     await index.initialize({
-        serverUrl: 'amqp://localhost'
+        serverUrl: 'amqp://localhost',
+        publishConfirmation: true
     })
     connectionManager.exchangeDeclare('test', 'topic', true)
     connectionManager.queueDeclare('test.inbound', true, false, {"x-message-ttl" : 20000})
     connectionManager.queueBind('test.inbound', 'test', '#')
 
-    connectionManager.publish('test', null, null, 'key', 'some message')
+    connectionManager.publish('test', 'testType', 'correlationId', 'key', 'some message')
 }
 
 initialize()
