163e994c431e019f13f86559f0af02d827839177
Gennaro Del Sorbo <JD78@JD-macbook.local>
2017-10-29 02:05:31 +0200

    publisher

--- test/integration.js
+++ test/integration.js
@@ -1,15 +1,23 @@
 const index = require('../lib/index')
 const connectionManager = require('../lib/connection-manager')
 
+let handlers = new Map()
+handlers.set('test', require('./test-message-handler'))
+
 let initialize = async () => {
     await index.initialize({
         serverUrl: 'amqp://localhost',
-        publishConfirmation: true
+        publishConfirmation: true,
+        handlers: handlers
     })
+
+    await connectionManager.initializeTopologyChannel()
+
     connectionManager.exchangeDeclare('test', 'topic', true)
     connectionManager.queueDeclare('test.inbound', true, false, {"x-message-ttl" : 20000})
     connectionManager.queueBind('test.inbound', 'test', '#')
 
+    await connectionManager.initializePublisher()
     connectionManager.publish('test', 'testType', 'correlationId', 'key', 'some message')
 }
 
---
+++ test/test-message-handler.js
@@ -0,0 +1,3 @@
+module.exports = (message, header) => {
+    console.log(message);
+}
