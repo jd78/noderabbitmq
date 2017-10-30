c2aa760c8fc042852069297caa5ac4933b5ba74f
Gennaro Del Sorbo <gennaro.delsorbo@gmail.com>
2017-10-30 16:06:10 +0100

    Consume messages in sequence

--- test/publisher.js
+++ test/publisher.js
@@ -1,26 +1,17 @@
 const index = require('../lib/index')
 const connectionManager = require('../lib/connection-manager')
 
-let handlers = new Map()
-handlers.set('testType', require('./test-message-handler'))
-
 let initialize = async () => {
     await index.initialize({
         serverUrl: 'amqp://localhost',
         publishConfirmation: true,
-        prefetch: 1000,
-        handlers: handlers
     })
 
     await connectionManager.initializeTopologyChannel([
-        { op: 'exchangeDeclare', name: 'test', type: 'topic', durable: true },
-        { op: 'queueDeclare', name: 'test.inbound', durable: true, autodelete: false, options: { "x-message-ttl": 20000 } },
-        { op: 'queueBind', queueName: 'test.inbound', exchangeName: 'test', routingKey: '#' }
+        { op: 'exchangeDeclare', name: 'test', type: 'topic', durable: true }
     ])
 
     await connectionManager.initializePublisher()
-
-    connectionManager.startConsumer('test.inbound')
 
     for(let i=0; i<100;i++){
         let testObj = {
--- test/test-message-handler.js
+++ test/test-message-handler.js
@@ -1,3 +1,11 @@
-module.exports = (message, header) => {
-    console.log(message);
+let asyncLog = m => new Promise((resolve, reject) => {
+    var randomnumber = Math.floor(Math.random() * (20 - 5 + 1)) + 5;
+    setTimeout(() => {
+        console.log(m)
+        resolve()
+    }, randomnumber)
+})
+
+module.exports = async (message, header) => {
+    await asyncLog(message)
 }
