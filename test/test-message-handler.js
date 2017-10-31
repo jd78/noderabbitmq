fa2c9ef3c13a3e20c15e83784c5423e4359f4ef7
Gennaro Del Sorbo <gennaro.delsorbo@gmail.com>
2017-10-31 16:25:56 +0100

    Exception handling

--- test/test-message-handler.js
+++ test/test-message-handler.js
@@ -1,3 +1,5 @@
+const lib = require('../lib/index')
+
 let asyncLog = m => new Promise((resolve, reject) => {
     var randomnumber = Math.floor(Math.random() * (20 - 5 + 1)) + 5;
     setTimeout(() => {
@@ -8,5 +10,8 @@ let asyncLog = m => new Promise((resolve, reject) => {
 
 module.exports = async (message, header) => {
     await asyncLog(message)
+
+    //throw lib.rejectMessageError
+
     //console.log(message)
 }
